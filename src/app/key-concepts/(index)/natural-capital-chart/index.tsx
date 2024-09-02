0251c3562163853fda3ddabb998e2bf32641935a
Clément Prod'homme <contact@clementprodhomme.fr>
2024-09-02 16:57:42 +0200

    Add tooltip content

--- src/app/key-concepts/(index)/natural-capital-chart/index.tsx
+++ src/app/key-concepts/(index)/natural-capital-chart/index.tsx
@@ -23,14 +23,20 @@ const NaturalCapitalChart = () => {
     <div className="flex flex-col gap-10 text-white">
       <div className="flex items-center justify-center gap-[10px]">
         <div className="text-4xl">Natural Capital Assets</div>
-        <InfoTooltip theme="dark" content="TBD" />
+        <InfoTooltip
+          theme="dark"
+          content="Biotic and abiotic assets in an ecosystem that can be described in terms of their extant and condition."
+        />
       </div>
       <div ref={chartContainerRef}>
         <NaturalCapitalChartSvg width={width} />
       </div>
       <div className="flex items-center justify-center gap-[10px]">
         <div className="text-4xl">Flow of services</div>
-        <InfoTooltip theme="dark" content="TBD" />
+        <InfoTooltip
+          theme="dark"
+          content="The products or processes arising from nature that are of benefit and value to human society."
+        />
       </div>
     </div>
   );
