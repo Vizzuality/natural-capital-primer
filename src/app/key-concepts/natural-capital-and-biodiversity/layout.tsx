e5da174484da6ad3b4bae3670f3ec715b79135f0
Clément Prod'homme <contact@clementprodhomme.fr>
2024-09-19 14:11:19 +0200

    Remove unneeded assets

--- src/app/key-concepts/natural-capital-and-biodiversity/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Natural Capital and Biodiversity",
-};
-
-export default function BiodiversityLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
--- src/app/key-concepts/natural-capital-and-climate-change/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Natural Capital and Climate Change",
-};
-
-export default function ClimateChangeLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
